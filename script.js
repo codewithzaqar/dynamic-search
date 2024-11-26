59af3df7ad0a629081189391b1ef44f127f40a9c
PerfectDevloper <hakobyanzaqar15@gmail.com>
2024-11-26 16:07:53 +0400

    v0.0.1.0

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function searchGoogle(event) {
-    event.preventDefault()
+    event.preventDefault() // Prevent page reload
     const query = document.getElementById('searchBox').value.trim()
     if (query) {
         window.location.href = `https://www.google.com/search?q=${encodeURIComponent(query)}`
     } else {
+        // Alert if no query is provided
         alert('Please enter a search query.')
     }
 }
